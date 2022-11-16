22c03a8ca8ca00a6648d2235d86f80d132d7bc3e
Ishank <Ishank@10.12.65.127>
2022-11-16 17:03:06 +0530

    jUnitTest

---
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -0,0 +1,9 @@
+package jUnitTestPackage; 
+public class jUnitFunctions {
+    public int addNumbers(int numl, int num2){ 
+        return numl+num2;
+    }
+    public String addStrings(String s1, String s2){
+        return s1+s2; 
+    }
+}
